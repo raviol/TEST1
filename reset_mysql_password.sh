b5b8bb6c1c62546d32b625efd33c2d19822d240c
QA Jelastic <andrey.kuznetsov@jelastic.com>
2017-04-27 18:08:35 +0300

    Create reset_mysql_password.sh

---
+++ reset_mysql_password.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo service mysql stop
+mysqld_safe --skip-grant-tables &
+sleep 5
+mysql -uroot -e "use mysql;update user set authentication_string=PASSWORD('123456') where User='root';flush privileges"
+sudo service mysql stop
+sudo service mysql start
+mysql -uroot -p123456 -e "create database pageone"
+mysql -uroot -p123456 pageone < pages.sql
