764590651ecd3af79d64da30a40a25fded42588c
QA Jelastic <andrey.kuznetsov@jelastic.com>
2017-04-27 19:07:58 +0300

    Update edit_database_json.sh

--- edit_database_json.sh
+++ edit_database_json.sh
@@ -1,3 +1,3 @@
-sed -i 's/127.0.0.1/$1/g' /opt/shared/webroot/ROOT/themes/pageone/settings/database.json
+sed -i "s/127.0.0.1/${1}/g" /opt/shared/webroot/ROOT/themes/pageone/settings/database.json
 sed -i 's/""/"123456"/g' /opt/shared/webroot/ROOT/themes/pageone/settings/database.json
 sed -i 's/"scripts":{/"scripts":{"start":"cofee app.coffee",/g' /opt/shared/webroot/ROOT/package.json
