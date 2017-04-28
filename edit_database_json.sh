e34bee082c1a9476effacb7d350642c8627ce858
QA Jelastic <andrey.kuznetsov@jelastic.com>
2017-04-28 10:25:28 +0300

    Update edit_database_json.sh

--- edit_database_json.sh
+++ edit_database_json.sh
@@ -1,3 +1,5 @@
 sed -i "s/127.0.0.1/${1}/g" /opt/shared/webroot/ROOT/themes/pageone/settings/database.json
 sed -i 's/""/"123456"/g' /opt/shared/webroot/ROOT/themes/pageone/settings/database.json
 sed -i 's/"scripts":{/"scripts":{"start":"coffee app.coffee",/g' /opt/shared/webroot/ROOT/package.json
+mysql -h${1} -uroot -p123456 -e "create database pageone"
+mysql -h${1} -uroot -p123456 pageone < pages.sql
